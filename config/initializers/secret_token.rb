fab58e11168826ed16398309155b6dfc1b7b6595
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2013-06-05 23:20:43 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breedercms::Application.config.secret_token = 'bff3139116f2409ff518bc0331e81b4176ab8e56a5ac95e521158d0d1f755db9913af8d6ae17db212f2bb43e79ed1a7eb254476879e717cec20b0874299aa966'
