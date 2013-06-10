6b73401641535cc383a7b91e98df86d50458473f
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2013-06-10 15:55:17 -0400

    use more meaningful routes for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,41 +3,41 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Breeder CMS'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Breeder CMS')
     end
     
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Breeder CMS")
     end
 
     it "should not have the custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "- Home")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the correct title" do
-      visit '/static_pages/about' 
+      visit about_path
       page.should have_selector('title', :text => '- About Us')
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact Us')
     end
 
     it "should have the correct title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => '- Contact Us')
     end
   end
