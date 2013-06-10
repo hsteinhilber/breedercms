60e35cd48a1eeccc3245f60a067deb332943358b
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2013-06-10 14:55:53 -0400

    improved layout of home screen

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,14 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Breeder CMS')
     end
     
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "- Home")
+      page.should have_selector('title', :text => "Breeder CMS")
+    end
+
+    it "should not have the custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "- Home")
     end
   end
 
