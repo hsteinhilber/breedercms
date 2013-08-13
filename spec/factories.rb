36b2a6f553f5b264fd0dc68691927720617ba2b9
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2013-08-13 10:21:50 -0400

    added attributes to pet profile page

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
     birth_date '2011-12-28'
     profile_picture File.open('./spec/support/Tapanga.jpg')
     gender     'F'
+    color      'Silver'
+    eye_color  'Brown'
+    breed      'Siberian Husky'
+    height     23
+    weight     53.5
   end
 end
--- spec/requests/pet_pages_spec.rb
+++ spec/requests/pet_pages_spec.rb
@@ -11,6 +11,13 @@ describe "PetPages" do
     it { should have_selector('title', text: pet.name) }
     it { should have_selector('h2', text: pet.birth_date.to_s) }
     it { should have_selector('img', url: pet.profile_picture.thumb.url) }
+
+    it { should have_selector('dd', text: pet.gender) }
+    it { should have_selector('dd', text: pet.color) }
+    it { should have_selector('dd', text: pet.eye_color) }
+    it { should have_selector('dd', text: pet.breed) }
+    it { should have_selector('dd', text: pet.height.to_s) }
+    it { should have_selector('dd', text: pet.weight.to_s) }
   end
   
   describe "pet registration page" do
