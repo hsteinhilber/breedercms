bbdc8ca4fce11cd2766a754a62ef9fa2447f6b7a
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2014-03-21 09:16:52 -0400

    first revision of home page for sundown site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return the full title on a per-page basis
   def full_title(page_title)
-    base_title = "Breeder CMS" # replace on a site-by-site basis 
+    base_title = "Sundown Siberian Huskies" # replace on a site-by-site basis 
     if page_title.empty? 
       base_title
     else
