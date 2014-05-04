3d833713e3b7dfd98fd550b9aa655ba38b602b15
Harry Steinhilber, Jr. <harry.steinhilber@gmail.com>
2014-05-03 20:01:58 -0400

    additional validations and methods for page model

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -14,4 +14,24 @@
 class StaticPage < ActiveRecord::Base
   attr_accessible :content, :subtitle, :title
   attr_readonly :slug
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :slug, uniqueness: true
+
+  def to_param
+    slug
+  end
+
+  def self.find_by_param(input)
+    input.to_s == input.to_i.to_s ? find_by_id(input) : find_by_slug(input)
+  end
+
+  def title=(v)
+    self.slug = v.gsub(/\s*&\s*/, "-and-")
+                 .gsub(/\s*@\s*/, "-at-")
+                 .gsub(/[`']/, "")
+                 .parameterize if self.new_record?
+    super(v)
+  end
 end
--- spec/models/static_page_spec.rb
+++ spec/models/static_page_spec.rb
@@ -14,7 +14,7 @@
 require 'spec_helper'
 
 describe StaticPage do
-  before { @page = StaticPage.new }
+  before { @page = StaticPage.new(title: "Example Page", content: "<p>This is some text.</p>") }
 
   subject { @page }
   
@@ -22,18 +22,73 @@ describe StaticPage do
 
   it { should respond_to(:title) }
   it { should respond_to(:title=) }
+  describe "when title is not present" do
+    before { @page.title = " " }
+    it { should_not be_valid }
+  end
 
   it { should respond_to(:subtitle) }
   it { should respond_to(:subtitle=) }
 
   it { should respond_to(:content) }
   it { should respond_to(:content=) }
+  describe "when content is not present" do
+    before { @page.content = " " }
+    it { should_not be_valid }
+  end
 
   it { should respond_to(:slug) }
   it { should respond_to(:slug=) }
   it "should not allow access to slug via mass assignment" do
     expect { subject.update_attributes(slug: "a-new-value") }.to raise_error
   end
+  its(:slug) { should eql("example-page") }
+  describe "when slug is already taken" do
+    before do
+      page_with_dup_title = @page.dup
+      page_with_dup_title.save
+    end
+    it { should_not be_valid }
+  end
+  describe "when changing the title of a page" do
+    describe "when the page has already been saved" do
+      before do
+        @page.save
+        @page.title = "Some New Title"
+      end
+      its(:slug) { should eql("example-page") }
+    end
+    describe "when the page has not been saved" do
+      before { @page.title = "Some New Title" }
+      its(:slug) { should eql("some-new-title") }
+    end
+  end
+  describe "when title contains ampersands (&)" do
+    before { @page.title = "This Title & That Title" }
+    its(:slug) { should eql("this-title-and-that-title") }
+  end
+  describe "when title contains at (@)" do
+    before { @page.title = "A Title @ Home" }
+    its(:slug) { should eql("a-title-at-home") }
+  end
+  describe "when title contains other special characters" do
+    before { @page.title = "`A Test's Title - *Special* Characters`" }
+    its(:slug) { should eql("a-tests-title-special-characters") }
+  end
 
-
+  it { should respond_to(:to_param) }
+  its(:class) { should respond_to(:find_by_param) }
+  describe "when searching for a page by param" do
+    before { @page.save }
+    it "should find the page using its id" do
+      StaticPage.find_by_param(@page.id).should eql(@page)
+    end
+    it "should return nil if page does not exist" do
+      StaticPage.find_by_param(-99).should be_nil
+    end
+    it "should find the page using its slug" do
+      StaticPage.find_by_param(@page.slug).should eql(@page)
+    end
+  end
+  its(:to_param) { should eql(@page.slug) }
 end
